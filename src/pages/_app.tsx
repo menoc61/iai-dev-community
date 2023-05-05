42ce0c468d8324d7e30590f5d2a310fac73d0e82
menoc61 <gillemomeni@gmail.com>
2023-05-05 14:42:44 +0100

    header befor first deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,18 @@ import { theme } from "../chakra/theme";
 import Layout from "../components/Layout";
 import "../firebase/clientApp";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme}>
         <Layout>
+          <Head>
+            <title>R/community</title>
+            <link rel="shortcut icon" href="/images/redditFace.svg" type="image/x-icon" />
+          </Head>
+
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
